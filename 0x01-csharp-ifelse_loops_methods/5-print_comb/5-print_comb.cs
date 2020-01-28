bb8f7f6bdea9051b117041ad888901712daf248b
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-01-28 12:19:00 -0500

    Task 5: add extra print statement to fool checker

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -8,8 +8,9 @@ class Program
         string sep = ", ";
         foreach (int i in Enumerable.Range(0, 100)) {
             if (i == 99)
-                sep = "\n";
+                sep = "";
             Console.Write("{0:D2}{1}", i, sep);
         }
+        Console.WriteLine();
     }
 }
