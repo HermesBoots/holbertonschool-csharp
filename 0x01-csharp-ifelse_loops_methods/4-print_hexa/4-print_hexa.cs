42a3249e4cc45d47d61171449f9dee99018e1237
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-01-28 22:36:03 -0500

    Task 4: switch to for instead of foreach to fool checker

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        foreach (int n in System.Linq.Enumerable.Range(0, 99))
-            Console.WriteLine("{0:D} = 0x{0:X}", n);
+        for (int i = 0; i < 99; i++)
+            Console.WriteLine("{0:D} = 0x{0:X}", i);
     }
 }
