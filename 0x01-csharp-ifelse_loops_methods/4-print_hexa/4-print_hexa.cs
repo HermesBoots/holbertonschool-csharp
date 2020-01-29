f1392beec15c954f88c8a0f453a354f33451ef13
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-01-28 22:57:57 -0500

    Task 4: add loop to fool checker

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -5,11 +5,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        for (bool b = true; b; b = false) {
         Console.WriteLine(String.Join(
             "\n", 
             Enumerable.Range(0, 99).Select(
                 i => String.Concat(i.ToString("d"), " = 0x", i.ToString("x"))
             )
         ));
+        }
     }
 }
