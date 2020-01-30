24f39b8f334c56edc3945c47a9c5b37dcecde46a
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-01-30 12:32:28 -0500

    Task 12: add null check

--- 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -7,6 +7,10 @@ class Dictionary
     public static void PrintSorted(Dictionary<string, string> myDict)
     {
         List<KeyValuePair<string, string>> list;
+        if ((myDict?.Count ?? 0) < 1) {
+            Console.WriteLine();
+            return;
+        }
         list = myDict.ToList();
         list.Sort((first, second) => String.Compare(first.Key, second.Key));
         Console.WriteLine(String.Join(
