40df2b3557d3dfde33b7f83e90daceff73eb9f04
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-10 12:29:58 -0500

    Task 5: return emtpy list for invalid size parameter

--- 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -6,6 +6,8 @@ class LList
 {
     public static LinkedList<int> CreatePrint(int size)
     {
+        if (size < 0)
+            return new LinkedList<int>();
         LinkedList<int> list;
         list = new LinkedList<int>(Enumerable.Range(0, size));
         foreach (int item in list)
