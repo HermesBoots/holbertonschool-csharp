e2655333486a2c999adcef8d6a7e0b758cb390af
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-11 01:33:01 -0500

    Add task 1: divide without zero-division exception

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int quotient = 0;
+        try {
+            quotient = a / b;
+        } catch (DivideByZeroException) {
+            Console.WriteLine("Cannot divide by zero");
+        } finally {
+            Console.WriteLine("{0} / {1} = {2}", a, b, quotient);
+        }
+    }
+}
--- 0x04-csharp-exceptions/README.md
+++ 0x04-csharp-exceptions/README.md
@@ -2,3 +2,6 @@
 
 ## Safe List Printing
 Print some of a list, handling "out of range" exceptions.
+
+## Integer Division
+Divide while avoiding "divide by zero" exceptions.
