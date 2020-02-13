2210d11e8b4a1c76871f1bd5ca1e5323e7cc9ed6
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-13 02:58:51 -0500

    Add task 1: add field and constructor to class

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,12 @@
+namespace Enemies
+{
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
--- 0x06-csharp-classes/README.md
+++ 0x06-csharp-classes/README.md
@@ -2,3 +2,6 @@
 
 ## Braaainsss
 Create a class within a namespace.
+
+## Healthy Competition
+Add a field and a constructor to a class.
