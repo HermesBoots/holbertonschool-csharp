c71e289087e0a02b0d3c7a61d39b44f30ac6d43d
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-13 22:55:23 -0500

    Task 0: remove extra comments

--- 0x06-csharp-classes/0-enemy/0-enemy.cs
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -1,8 +1,5 @@
-// Holds all enemy types.
-/// <summary>Holds all enemy types.</summary>
 namespace Enemies
 {
-    // Records health of an enemy zombie.
     /// <summary>Records health of an enemy zombie.</summary>
     public class Zombie
     {
