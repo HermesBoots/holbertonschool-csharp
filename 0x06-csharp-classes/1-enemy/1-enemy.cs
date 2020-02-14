88013e578f1ff3bdf55dc620135e8b164e0f85da
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-13 22:58:40 -0500

    Task 1: add documentation

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -1,9 +1,12 @@
 namespace Enemies
 {
+    /// <summary>Records health of an enemy zombie.</summary>
     class Zombie
     {
+        /// <summary>The zombie's remaining health points.</summary>
         public int health;
 
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class with a default health of 0.</summary>
         public Zombie()
         {
             this.health = 0;
