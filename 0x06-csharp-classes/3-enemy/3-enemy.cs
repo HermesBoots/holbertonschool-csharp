e00de47d5ec2460a794d24c280938b8b2bb38190
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-13 23:04:15 -0500

    Task 3: add documentation

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -2,14 +2,19 @@ using System;
 
 namespace Enemies
 {
+    /// <summary>Represents a zombie enemy.</summary>
     class Zombie
     {
+        // remaining health points
         private int health;
 
+        /// <summary>Initializes a new <see cref="Zombie"/> instance with default health.</summary>
         public Zombie()
         {
             this.health = 0;
         }
+
+        /// <summary>Initializes a new <see cref="Zombie"/> instance with specified health.</summary>
         public Zombie(int value)
         {
             if (value < 0)
@@ -17,6 +22,8 @@ namespace Enemies
             this.health = value;
         }
 
+        /// <summary>Gets the zombie's remaining health.</summary>
+        /// <returns>The zombie's remaining health.</returns>
         public int GetHealth()
         {
             return this.health;
