0dd4cc0f8bb31f8e9688dd43ce8fb04c1d22e5f7
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-02 03:18:24 -0500

    Task 0: fix malformed XML end tag

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -3,7 +3,7 @@ class Obj
 {
     /// <summary>Check whether an object is an <c>int</c>.</summary>
     /// <param name="obj">Object to check.</param>
-    /// <returns>Whether <paramref name="obj"/> is an <c>int</c>.</summary>
+    /// <returns>Whether <paramref name="obj"/> is an <c>int</c>.</returns>
     public static bool IsOfTypeInt(object obj)
     {
         return obj.GetType() == typeof(int);
