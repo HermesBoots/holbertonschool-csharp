7b7260cf2d557461114b7cd68c5eceb15043576e
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-09 01:57:28 -0400

    Add documentation to tasks 3 and 6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,7 +1,12 @@
 using System;
 
+/// <summary>Contains operations to perform on vectors.</summary>
 class VectorMath
 {
+    /// <summary>Add two vectors.</summary>
+    /// <param name="vector1">First vector.</param>
+    /// <param name="vector2">Second vector.</param>
+    /// <returns>Sum of vectors.<returns>
     public static double[] Add(double[] vector1, double[] vector2) {
         if (vector1 is null || vector1.Rank != 1 || vector1.Length < 2 || vector1.Length > 3)
             return new double[] { -1 };
