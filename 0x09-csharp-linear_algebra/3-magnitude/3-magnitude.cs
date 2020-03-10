a2216726c9837bd55f5c09d899401665f99a137b
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-10 18:12:43 -0400

    Task 3: add rounding to result

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -9,6 +9,11 @@ class VectorMath
     public static double Magnitude(double[] vector) {
         if (vector is null || vector.Rank != 1 || vector.Length < 2 || vector.Length > 3)
             return -1;
-        return Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector.Length > 2 ? vector[2] : 0, 2));
+        double ret = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+        if (vector.Length > 2)
+            ret += Math.Pow(vector[2], 2);
+        ret = Math.Sqrt(ret);
+        ret = Math.Round(ret, 1);
+        return ret;
     }
 }
