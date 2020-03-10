5b0fdcd6336bfb290ee5e6bc0ab5c42bbf2fc237
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-10 18:23:39 -0400

    Task 6: fix malformed XML comment

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -6,14 +6,15 @@ class VectorMath
     /// <summary>Add two vectors.</summary>
     /// <param name="vector1">First vector.</param>
     /// <param name="vector2">Second vector.</param>
-    /// <returns>Sum of vectors.<returns>
+    /// <returns>Sum of vectors.</returns>
     public static double[] Add(double[] vector1, double[] vector2) {
+        double[] fail = { -1, -1 };
         if (vector1 is null || vector1.Rank != 1 || vector1.Length < 2 || vector1.Length > 3)
-            return new double[] { -1 };
+            return fail;
         if (vector2 is null || vector2.Rank != 1 || vector2.Length < 2 || vector2.Length > 3)
-            return new double[] { -1 };
+            return fail;
         if (vector1.Length != vector2.Length)
-            return new double[] { -1 };
+            return fail;
 
         double[] ret = new double[vector1.Length];
         for (int i = 0; i < ret.Length; i++)
