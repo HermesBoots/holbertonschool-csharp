4f67b5e5b8c577b8430fd3a559b2b4bb9f264aa1
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-10 18:08:14 -0400

    Task 3: fix malformed XML comment

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,7 +4,7 @@ using System;
 class VectorMath
 {
     /// <summary>Find the magnitude of a vector.</summary>
-    /// <param name="vector">The two coordinates of the vector destination, assuming a source of (0, 0[, 0]).<param>
+    /// <param name="vector">The two coordinates of the vector destination, assuming a source of (0, 0[, 0]).</param>
     /// <returns>The length/magnitude of the vector.</returns>
     public static double Magnitude(double[] vector) {
         if (vector is null || vector.Rank != 1 || vector.Length < 2 || vector.Length > 3)
