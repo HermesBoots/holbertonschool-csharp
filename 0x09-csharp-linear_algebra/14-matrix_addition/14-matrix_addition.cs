0878ed9c89827136bb19293999a10851474629c0
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-11 03:05:20 -0400

    Task 14: change to handle only 2x2 or 3x3 arrays

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,15 +10,13 @@ class MatrixMath
     /// <param name="matrix2">Second matrix.</param>
     /// <returns>Sum of the matrices.</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2) {
-        if (matrix1 is null || matrix1.Rank < 2 || matrix1.Rank > 3)
-            return new double[,] { { -1 } };
-        if (matrix2 is null || matrix2.Rank < 2 || matrix2.Rank > 3)
-            return new double[,] { { -1 } };
-        if (matrix1.Rank != matrix2.Rank)
-            return new double[,] { { -1 } };
-        for (int dimension = 0; dimension < matrix1.Rank; dimension++)
-            if (matrix1.GetLength(dimension) != matrix2.GetLength(dimension))
-                return new double[,] { { -1 } };
+        double[,] fail = { { -1, -1 }, { -1, -1 } };
+        if (matrix1 is null || matrix1.Rank != 2 || !(matrix1.Length == 4 || matrix1.Length == 9))
+            return fail;
+        if (matrix2 is null || matrix2.Rank != 2 || !(matrix2.Length == 4 || matrix2.Length == 9))
+            return fail;
+        if (matrix1.Length != matrix2.Length || matrix1.GetLength(0) != matrix2.GetLength(0))
+            return fail;
 
         double[,] ret = (double[,])matrix1.Clone();
         for (int x = 0; x < matrix1.GetLength(0); x++)
