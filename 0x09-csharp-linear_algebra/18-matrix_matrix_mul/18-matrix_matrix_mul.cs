0e1f65219082a55b3a62c73580ff8b73593b41e5
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-11 04:02:28 -0400

    Task 18: swap X and Y for dimensions of result matrix

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -8,9 +8,9 @@ class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2) {
         if (matrix1 is null || matrix2 is null)
             return new double[,] { { -1, -1 }, { -1, -1 } };
-        if (matrix1.GetLength(0) != matrix2.GetLength(1))
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
             return new double[,] { { -1, -1 }, { -1, -1 } };
-        double[,] ret = new double[matrix1.GetLength(1), matrix2.GetLength(0)];
+        double[,] ret = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
         for (int y = 0; y < ret.GetLength(0); y++)
             for (int x = 0; x < ret.GetLength(1); x++) {
                 double sum = 0;
